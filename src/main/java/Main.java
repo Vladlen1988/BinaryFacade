ce980f4fb9c28282baff075ba847f555bf13aac1
Vladlen Yamalov <v011088@yandex.ru>
2022-10-17 23:53:57 +0700

    add files

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps binOps = new BinOps();
+        System.out.println(binOps.sum("10100", "11000"));
+        System.out.println(binOps.mult("1010", "101"));
+    }
+}
